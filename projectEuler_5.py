c00ccc14c4fc77accb900e6e4ad1887e0e94d713
makutene <38430078+makutene@users.noreply.github.com>
2018-05-01 14:58:51 +0200

    Create projectEuler_5.py

---
+++ projectEuler_5.py
@@ -0,0 +1,16 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+def isDivisible(num):
+  for i in range(1, 21):
+    if num % i !=0:
+      return False
+  return True
+  
+num=1
+while True:
+  if isDivisible(num):
+    break
+  num+=1
+
+print(num)
