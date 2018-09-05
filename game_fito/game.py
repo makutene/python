d967831cd3f03d6697aabb58f6b2897881de7feb
makutene <38430078+makutene@users.noreply.github.com>
2018-09-05 19:45:41 +0200

    Update game.py

--- game_fito/game.py
+++ game_fito/game.py
@@ -15,54 +15,32 @@ rooms={
         'R15':{
                 'este':'pasillo hacia R15',
                 'sur':'salida hacia una vida mejor!',
-                'suelo':['pala']}
-        
-        }        
-        
+                'suelo':['pala']}}
+
+
 
 class Char:
-  def __init__(self):
-    self.name=''
-    self.moral=1
-    self.moral_max=1
+  def __init__(self, name, moral, moral_max):
+    self.name= name
+    self.moral= 10
+    self.moral_max= 10
 
 class Player(Char):
-  def __init__(self):
-    Char.__init__(self)
-    self.moral=10
-    self.moral_max=10
+  def __init__(self,name, moral, moral_max, state):
+    super().__init__(name, moral, moral_max)
+    self.state= state
+  
   def status(self):
-    print('%s moral: %d/%d' % (self.name,self.moral, self.moral_max))
-  def quit(self):
-    print('%s has died' % self.name)
-  def printLoc(loc):
-    print(loc)
-  def move(direc):
+    print('%s moral is: %d/%d' % (self.name, self.moral, self.moral_max))
+  
+  def move(self, direc):
     global currentLocation
     if direc in rooms[currentLocation]:
-      currentLocation=rooms[currentLocation][direc]        
-      
+      currentLocation= rooms[currentLocation][direc]
+      print('%s is at: %s' % (self.name, currentLocation))
+    else:
+      print('u cant move there')
 
 
-comandos={'status':Player.status,
-          'quit':Player.quit,
-          'loc':Player.printLoc,
-          'n':Player.move('norte'),
-          's':Player.move('sur'),
-          'e':Player.move('este'),
-          'o':Player.move('oeste')}
 
 
-p= Player()
-p.name=input('type your name: >')
-
-while (p.moral > 0):
-  line= input('>')
-  args = line.split()
-  if len(args) > 0:
-    commandFound = False
-    for c in comandos.keys():
-      if args[0] == c[:len(args[0])]:
-        comandos[c](p)
-        commandFound = True
-        break
